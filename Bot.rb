d58a31fbbcb6825300f8d2ba9a22defd6ead097b
Lakrouch <kirill.zelenkov@yandex.by>
2022-04-15 15:26:39 +0300

    Simple version is done

--- Bot.rb
+++ Bot.rb
@@ -1,23 +1,20 @@
 require 'telegram/bot'
 
 class Bot
-	attr_accessor :mistakes, :points, :key, :request, :bot
+	attr_accessor :mistakes, :points, :key, :bot, :statement
+
 	def initialize
-		@mistakes = 5
+		@mistakes = 2
 		@points = 0
 		@key = '5217763625:AAF79QpWHdGlfueS4dQ0nsOzVFdZHZwrS6E'.freeze
-		@request = ''
 		@current_path = File.dirname(__FILE__)+"/Data"
 		@photo_path = @current_path+"/Persons_photos/"
 	end
 
-	def play
-
-	end
-
 	def send_photo(message)
 		photo = get_photo
-		bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new(@photo_path+photo, 'image/jpeg'))
+		bot.api.send_photo(chat_id: message.chat.id,
+		                   photo: Faraday::UploadIO.new(@photo_path+photo, 'image/jpeg'))
 	end
 
 	def get_photo
@@ -26,11 +23,17 @@ class Bot
 		file.each do |line|
 			persons<<line
 		end
-		person = persons[rand(0...persons.length)].split(',')
-		return person[2]
+		@person = persons[rand(0...persons.length)].split(',')
+		@person[0] == 'Актёр' ? @statement = 1 : @statement= 0
+		return @person[2]
 	end
 
-	def say(message,text)
+	def say(message,text=@person[1])
 		@bot.api.send_message(chat_id: message.chat.id, text: text)
+	end
+
+	def destroy
+		@mistakes = 2
+		@points = 0
 	end
 end
--- BotStart.rb
+++ BotStart.rb
@@ -8,9 +8,41 @@ Telegram::Bot::Client.run(bot_play.key) do |bot|
     when '/start'
 	    bot_play.say(message, "Hello, #{message.from.first_name}")
 	    bot_play.send_photo(message)
-      answers =
-        Telegram::Bot::Types::ReplyKeyboardMarkup
-          .new(keyboard: [%w(Актёр Прогер)], one_time_keyboard: true)
+	    Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [%w(Актёр Прогер)], one_time_keyboard: true)
+    when 'Актёр'
+	    if bot_play.statement == 1
+		    bot_play.say(message, 'Ты прав!')
+		    bot_play.say(message)
+		    bot_play.points += 1
+	    else
+		    bot_play.say(message, 'Ты не прав!')
+		    bot_play.mistakes -= 1
+		    if bot_play.mistakes == 0
+			    bot_play.say(message, "Ты проиграл со счётом #{bot_play.points}")
+			    bot_play.destroy
+			    break
+		    end
+	    end
+	    bot_play.send_photo(message)
+    when 'Прогер'
+	    if bot_play.statement == 0
+		    bot_play.say(message, 'Ты прав!')
+		    bot_play.say(message)
+		    bot_play.points += 1
+	    else
+		    bot_play.say(message, 'Ты не прав!')
+		    bot_play.mistakes -= 1
+		    if bot_play.mistakes == 0
+			    bot_play.say(message, "Ты проиграл со счётом #{bot_play.points}")
+			    bot_play.destroy
+			    break
+		    end
+	    end
+	    bot_play.send_photo(message)
+    when '/stop'
+	    bot_play.say(message, 'Bye!')
+	    bot_play.destroy
+	    break
     end
   end
 end
